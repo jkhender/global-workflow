fbc762a090bf999e9be342afbb048c0fbc4d9c25
Judy.K.Henderson <Judy.K.Henderson@noaa.gov>
2020-10-23 18:55:27 +0000

    - reverted back to older modules to see if netcdf HDF errors go away  - changed to 17Sep develop ufs-weather-model branch, 2a6528d

--- sorc/checkout.sh
+++ sorc/checkout.sh
@@ -39,13 +39,16 @@ if [[ ! -d fv3gfs_ccpp.fd ]] ; then
     #JKHgit clone --recursive -b gsd/develop https://github.com/NOAA-GSD/ufs-weather-model  fv3gfs_ccpp.fd >> ${topdir}/checkout-fv3gfs_ccpp.log 2>&1
     #JKHcd fv3gfs_ccpp.fd
     #JKHgit checkout eff83ae37d98c66534366ba3c8d773b9522a5c62
-    git clone https://github.com/ufs-community/ufs-weather-model ufs-weather-model-b00a11f  >> ${topdir}/checkout-ufs-b00a11f.log 2>&1
-    cd ufs-weather-model-b00a11f
-    git checkout b00a11f
+#JKH    git clone https://github.com/ufs-community/ufs-weather-model ufs-weather-model-b00a11f  >> ${topdir}/checkout-ufs-b00a11f.log 2>&1
+#JKH    cd ufs-weather-model-b00a11f
+#JKH    git checkout b00a11f
+    git clone https://github.com/ufs-community/ufs-weather-model ufs-weather-model >> ${topdir}/checkout-ufs-2a6528d.log 2>&1
+    cd ufs-weather-model
     git submodule sync
     git submodule update --init --recursive
     cd ${topdir}
-    ln -fs ufs-weather-model-b00a11f fv3gfs_ccpp.fd 
+#JKH    ln -fs ufs-weather-model-b00a11f fv3gfs_ccpp.fd 
+    ln -fs ufs-weather-model fv3gfs_ccpp.fd 
     #JKHln -fs fv3gfs_ccpp.fd fv3gfs.fd
 else
     echo 'Skip.  Directory fv3gfs_ccpp.fd already exists.'
